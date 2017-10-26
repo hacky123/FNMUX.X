fdefcf671d6e96e7aedc35b2bec16f7fa5b95696
sayan <sayan@github.com>
2017-10-26 11:31:43 +0530

    finished for first release

---
+++ header 1.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   header 1.h
+ * Author: DDC
+ * this file is created by sayan in the master
+ * Created on October 26, 2017, 11:30 AM
+ */
+
+#ifndef HEADER_1_H
+#define	HEADER_1_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* HEADER_1_H */
+
